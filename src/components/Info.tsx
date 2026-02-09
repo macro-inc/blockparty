5563653da8139730129c595321acae3883effab8
Fake.User <russellboswell@protonmail.com>
2026-02-09 13:27:48 -0500

    copy info

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,12 +1,71 @@
 import { SectionLabel } from "./SectionLabel";
+import { createSignal } from "solid-js";
+
 
 export function Info() {
+  const [emailText, setEmailText] = createSignal(<><span aria-hidden="true">??? </span>russell@macro.com</>);
+  const [dateText, setDateText] = createSignal(<>Friday Febuary 27 7pm - 11pm</>);
+
+  function openMaps() {
+    window.location.href = "maps:q=122+Central+Ave+Brooklyn+New+York";
+  }
+
+  function copyDate() {
+    setDateText(<>·····················copied!</>);
+    navigator.clipboard.writeText("Friday Febuary 27 7pm - 11pm");
+    setTimeout(() => {setDateText(<>Friday Febuary 27 7pm - 11pm</>)}, 500);
+  }
+
+  function copyEmail() {
+    setEmailText(<>··············copied!</>);
+    navigator.clipboard.writeText("russell@macro.com");
+    setTimeout(() => {setEmailText(<><span aria-hidden="true">??? </span>russell@macro.com</>)}, 500);
+  }
+
   return (
-    <div style="user-select: none;">
-      <SectionLabel text="Info" />
-      122 Central Ave, Brooklyn, NYC<br/>
-      Friday Febuary 27 7pm - 11pm<br/>
-      ??? russell@macro.com
-    </div>
+    <>
+      <style>{`
+        button{
+          transition: color var(--transition);
+          background-color: unset;
+          color: var(--c4);
+          cursor: pointer;
+          outline: none;
+          border: none;
+        }
+        @media(hover){
+          button:hover{
+            transition: none;
+            color: var(--a0);
+          }
+        }
+      `}
+      </style>
+      <div style="user-select: none;">
+        <SectionLabel text="Info" />
+
+        <button
+          aria-label="open maps"
+          onClick={openMaps}
+        >
+          122 Central Ave, Brooklyn, NYC
+        </button>
+
+        <button
+          aria-label="copy date"
+          onClick={copyDate}
+        >
+          {dateText()}
+        </button>
+
+        <button
+          onClick={copyEmail}
+          aria-label="copy email"
+          tabindex="0"
+        >
+          {emailText()}
+        </button>
+      </div>
+    </>
   );
 }
