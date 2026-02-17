ae409b812ad53a4b564d9c3ec2b6456bebf29acd
Fake.User <russellboswell@protonmail.com>
2026-02-17 14:28:31 -0500

    Update ToyMacro.tsx

--- src/components/ToyMacro.tsx
+++ src/components/ToyMacro.tsx
@@ -8,23 +8,62 @@ export function ToyMacro() {
         gap: 20px;
       "
     >
-      <div
-        style={{
-          '-webkit-mask-image': `url(${ditherM})`,
-          '-webkit-mask-mode': 'luminance',
-          '-webkit-mask-size': '100% 100%',
-          'mask-image': `url(${ditherM})`,
-          'background-color': 'var(--c4)',
-          'image-rendering': 'pixelated',
-          'justify-self': 'center',
-          'mask-mode': 'luminance',
-          'mask-size': '100% 100%',
-          'user-select': 'none',
-          'flex-shrink': '0',
-          'height': '155px', // 'height': '124px',
-          'width': '220px', // 'width': '176px',
-        }}
-      />
+      <svg fill="none" style="display: block; width: 100%;" stroke="var(--a0)" stroke-linecap="round" stroke-miterlimit="99" stroke-linejoin="round" stroke-width="0.2" viewBox="0 0 37 25" xmlns="http://www.w3.org/2000/svg">
+        <path   fill="var(--b1)" d="m20.51 12.501 3.3805-1.3255 12.693 11.996-3.378 1.3278z"/>
+        <path d="m23.89 11.176v4.5204"/>
+        <g transform="translate(0, 15)">
+          <g stroke-dasharray="0.5, 1">
+            <path d="m23.89 11.176-3.3805 1.3255"/>
+            <path d="m23.89 11.176v-10.674"/>
+            <path d="m23.89 11.176 12.693 11.996"/>
+          </g>
+          <path fill="oklch(from var(--a0) l c h / 0.5)" d="m23.89 0.5019-3.3805 1.3255v10.674l12.696 11.999 3.378-1.3278-1e-4 -10.674z"/>
+          <g>
+            <path d="m33.206 13.826-12.696-11.999"/>
+            <path d="m33.206 13.826 3.3779-1.3278"/>
+            <path d="m33.206 13.826v10.673"/>
+          </g>
+        </g>
+        <path fill="var(--b1)" stroke="none" d="m20.51 12.501-1.4402 0.0125v13.486h19v-2.8183l-1.486-0.0095-3.378 1.3278z"/>
+        <path d="m20.51 12.501 12.696 11.999 3.378-1.3278"/>
+
+        <path   fill="var(--b1)" d="m6.4402 12.501 3.3805-1.3255 12.693 11.996-3.378 1.3278z"/>
+        <path d="m9.8207 11.176v4.5204"/>
+        <g transform="translate(0, 15)">
+          <g stroke-dasharray="0.5, 1">
+            <path d="m9.8207 11.176-3.3805 1.3255"/>
+            <path d="m9.8207 11.176v-10.674"/>
+            <path d="m9.8207 11.176 12.693 11.996"/>
+          </g>
+          <path fill="oklch(from var(--a0) l c h / 0.5)" d="m9.8207 0.50169-3.3805 1.3255v10.674l12.696 11.999 3.378-1.3278-5.8e-5 -10.674z"/>
+          <g>
+            <path d="m19.136 13.826-12.696-11.999"/>
+            <path d="m19.136 13.826 3.3779-1.3279"/>
+            <path d="m19.136 13.826v10.673"/>
+          </g>
+        </g>
+        <path fill="var(--b1)" stroke="none" d="m6.4402 12.501-1.4402 0.0125v13.486h19v-2.8183l-1.486-0.0095-3.378 1.3278z"/>
+        <path d="m6.4402 12.501 12.696 11.999 3.378-1.3278"/>
+
+        <path   fill="var(--b1)" d="m0.41661 20.097 3.3805-1.3255 4.6567 4.3976-3.3834 1.33z"/>
+        <path d="m3.7971 18.772v4.5204"/>
+        <g transform="translate(0, 15)">
+          <g stroke-dasharray="0.5, 1">
+            <path d="m8.454 23.169-4.6569-4.3976"/>
+            <path d="m3.7971 18.772-3.3805 1.3255"/>
+            <path d="m3.7971 18.772v-10.673"/>
+          </g>
+          <path fill="oklch(from var(--a0) l c h / 0.5)" d="m3.7971 8.0984-3.3805 1.3255v10.673l4.6538 4.4021 3.3834-1.33v-10.673z"/>
+          <g>
+            <path d="m5.0704 13.826v10.673"/>
+            <path d="m5.0704 13.826-4.6538-4.4021"/>
+            <path d="m5.0704 13.826 3.3834-1.33"/>
+          </g>
+        </g>
+        <path fill="var(--b1)" stroke="none" d="m0.41661 20.097-1.4166 0.012184v5.8906h11v-2.8184l-1.5462-0.012319-3.3834 1.33z"/>
+        <path d="m0.41661 20.097 4.6538 4.4021 3.3834-1.33"/>
+      </svg>
+
       <div style="user-select: none;">
         <a href="https://macro.com/" target="_blank">Macro: Local-First Team OS</a><br/>
         <a href="https://macro.com/jobs/" target="_blank">Size: 14+ Developers</a><br/>
