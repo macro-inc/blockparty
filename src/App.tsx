c9fbf560397bfec37d7a3df7c77535e286b047cf
Fake.User <russellboswell@protonmail.com>
2026-02-17 19:38:17 -0500

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { currentThemeId, themeShouldMatchSystem, systemMode, darkModeTheme, lightModeTheme } from './theme/signals/themeSignals';
 import { applyTheme, systemThemeEffect } from './theme/utils/themeUtils';
 import { type Component, createEffect, on } from 'solid-js';
+import { updateFavicon } from './theme/utils/themeFavicon';
 import { themeUpdate } from './theme/signals/themeSignals';
 import { SideBar } from './components/SideBar';
-import { updateFavicon } from './utils/utils';
 import { Canvas } from './components/Canvas';
 
 const App: Component = () => {
---
+++ src/theme/utils/themeFavicon.ts
@@ -0,0 +1,9 @@
+const elementFavicon = document.getElementById('favicon')!;
+import { themeReactive } from "../signals/themeReactive";
+
+export function updateFavicon(){
+  elementFavicon.setAttribute(
+    'href',
+    `data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' stroke='oklch(${themeReactive.a0.l[0]()} ${themeReactive.a0.c[0]()} ${themeReactive.a0.h[0]()}deg)' fill='none' version='1.1' viewBox='0 0 24 24'%3E%3Ccircle cx='12' cy='12' r='12' fill='oklch(${themeReactive.b0.l[0]()} ${themeReactive.b0.c[0]()} ${themeReactive.b0.h[0]()}deg)' stroke='none'/%3E%3Ccircle cx='12' cy='12' r='8' /%3E%3Crect x='4' y='4' width='16' height='16'%3E%3CanimateTransform attributeName='transform' repeatCount='indefinite' keySplines='0.42 0 1 1' calcMode='spline' keyTimes='0; 1' from='0 12 12' type='rotate' to='90 12 12' dur='1.5s' /%3E%3C/rect%3E%3C/svg%3E`
+  );
+}
