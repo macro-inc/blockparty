8db258a8b3cc2c7d25392cccdae5bde97b14ae0c
Fake.User <russellboswell@protonmail.com>
2026-02-18 16:10:39 -0500

    fix

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -4,7 +4,7 @@ export function Description() {
   return (
     <div style="user-select: none;">
       <SectionLabel text="Blockparty Vol.2" />
-      7 develeopers × 6 minutes. <a href="https://secretrisoclub.com" target="_blank">SRC Lower Level</a>. Free screen printing designs by <a href="https://paal.world/" target="_blank">Paal World</a> and <a href="https://emilefortune.com/" target="_blank">Emile Fortune</a> (blanks for sale or byo). Presented by <a href="https://macro.com/" target="_blank">Macro</a>.
+      7 developers × 6 minutes. <a href="https://secretrisoclub.com" target="_blank">SRC Lower Level</a>. Free screen printing designs by <a href="https://paal.world/" target="_blank">Paal World</a> and <a href="https://emilefortune.com/" target="_blank">Emile Fortune</a> (blanks for sale or byo). Presented by <a href="https://macro.com/" target="_blank">Macro</a>.
     </div>
   )
 }
