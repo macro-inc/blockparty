7baca592d7298e008ea95f8e68372a8b12bcc9ad
Fake.User <russellboswell@protonmail.com>
2026-02-17 19:28:48 -0500

    favicon theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import { currentThemeId, themeShouldMatchSystem, systemMode, darkModeTheme, lightModeTheme } from './theme/signals/themeSignals';
 import { applyTheme, systemThemeEffect } from './theme/utils/themeUtils';
+import { type Component, createEffect, on } from 'solid-js';
+import { themeUpdate } from './theme/signals/themeSignals';
 import { SideBar } from './components/SideBar';
+import { updateFavicon } from './utils/utils';
 import { Canvas } from './components/Canvas';
-import type { Component } from 'solid-js';
 
 const App: Component = () => {
   systemThemeEffect();
   if (themeShouldMatchSystem()) {applyTheme(systemMode() === 'dark' ? darkModeTheme() : lightModeTheme())}
   else { applyTheme(currentThemeId()) }
+
+  createEffect(
+    on(
+      themeUpdate,
+      () => {updateFavicon()}
+    )
+  )
 
   return (
     <main id="main">
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,4 @@
+import { themeReactive } from "../theme/signals/themeReactive";
 import { createSignal } from "solid-js";
 
 const point = 1000;
@@ -9,3 +10,12 @@ window.addEventListener("resize", () => {
   if(window.innerWidth >= point){setBreakpoint(false)}
   else{setBreakpoint(true)};
 });
+
+const elementFavicon = document.getElementById('favicon')!;
+
+export function updateFavicon(){
+  elementFavicon.setAttribute(
+    'href',
+    `data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' stroke='oklch(${themeReactive.a0.l[0]()} ${themeReactive.a0.c[0]()} ${themeReactive.a0.h[0]()}deg)' fill='none' version='1.1' viewBox='0 0 24 24'%3E%3Ccircle cx='12' cy='12' r='12' fill='oklch(${themeReactive.b0.l[0]()} ${themeReactive.b0.c[0]()} ${themeReactive.b0.h[0]()}deg)' stroke='none'/%3E%3Ccircle cx='12' cy='12' r='8' /%3E%3Crect x='4' y='4' width='16' height='16'%3E%3CanimateTransform attributeName='transform' repeatCount='indefinite' keySplines='0.42 0 1 1' calcMode='spline' keyTimes='0; 1' from='0 12 12' type='rotate' to='90 12 12' dur='1.5s' /%3E%3C/rect%3E%3C/svg%3E`
+  );
+}
