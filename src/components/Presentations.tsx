ffdcf918fb31d6f58c95ea1a1f2d5dc24727f99b
Fake.User <russellboswell@protonmail.com>
2026-03-02 11:20:10 -0500

    teo

--- src/components/Presentations.tsx
+++ src/components/Presentations.tsx
@@ -13,12 +13,11 @@ export function Presentations() {
       <a href="https://scd31.com/" target="_blank" class="row"><div>Stephen Downward</div><hr/><div>DIY GPU</div></a>
       {/*<a href="https://wobblybits.blog/" target="_blank" class="row"><div>David Allen Feil</div><hr/><div>Wobbly Bits</div></a>*/}
       <a href="https://self.destruct.xxx/" target="_blank" class="row"><div>Russell Boswell</div><hr/><div>Ok</div></a>
-      {/*<a href="https://www.frankchiarulli.com/" target="_blank" class="row"><div>Frank Chiarulli</div><hr/><div>RCade</div></a>*/}
-      <a href="https://macro.com/" target="_blank" class="row"><div>Jacob Beckerman</div><hr/><div>Macro</div></a>
       {/*<a href="https://eieio.games/" target="_blank" class="row"><div>Nolen Royalty</div><hr/><div>SSH Snake</div></a>*/}
       <a href="https://charlie-volow.com/" target="_blank" class="row"><div>Charlie Volow</div><hr/><div>Jackdaw + Midi</div></a>
       <a href="https://minivac.greg.technology/" target="_blank" class="row"><div>Greg Sadetsky</div><hr/><div>Minivak</div></a>
       <a href="https://izz.ee/" target="_blank" class="row"><div>Issac Chabon</div><hr/><div>Calendar</div></a>
+      <a href="https://synoet.io/" target="_blank" class="row"><div>Teo Nys</div><hr/><div>55</div></a>
     </div>
   );
 }
